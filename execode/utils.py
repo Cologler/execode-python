40dec9eb7bd66a5ea52790358eb0e8bed846dd7c
Cologler <skyoflw@gmail.com>
2019-04-21 21:40:57 +0800

    refactor

--- execode/utils.py
+++ execode/utils.py
@@ -7,10 +7,15 @@
 
 import sys
 from typing import Union
+from collections import namedtuple
 
 from fsoopify import NodeType, NodeInfo, DirectoryInfo, FileInfo
 
 def find_pipfile(node: Union[NodeInfo, str]):
+    '''
+    find the first parent dir which has `Pipfile`, return the path of the `Pipfile`.
+    '''
+
     def _find_pipfile(dir: DirectoryInfo, deep: int):
         if deep == 0:
             return None
@@ -22,33 +27,77 @@ def find_pipfile(node: Union[NodeInfo, str]):
     if isinstance(node, str):
         node = NodeInfo.from_path(node)
         if not node:
-            raise RuntimeError(f'{node} is not exists')
+            raise FileNotFoundError(f'{node} is not exists')
+
     elif node.node_type == NodeType.file:
         node = node.get_parent()
 
     return _find_pipfile(node, 10)
 
-def find_pkg_root(node: Union[NodeInfo, str]):
-    def _find_from_dir(node: DirectoryInfo):
-        parent = node.get_parent()
-        init = parent.get_fileinfo('__init__.py')
-        if init.is_file():
-            parent_init_path = _find_from_dir(parent)
-            return parent_init_path if parent_init_path else init.path
+PyInfo = namedtuple('PyInfo', [
+    'path',
+    'pkg_root',
+    'pkg_name',
+    'name',
+])
 
-    def _file_find_root(file: FileInfo):
-        parent = file.get_parent()
-        init = parent.get_fileinfo('__init__.py')
-        if not init.is_file():
-            return file.path # the file is single file package
-        return _find_from_dir(file)
+def get_pyinfo(node: Union[NodeInfo, str]):
+    store = {
+        'pkg_root': None
+    }
+    pkg_parts = []
+    name_parts = []
+
+    def resolve_from_dir(node: DirectoryInfo):
+        init = node.get_fileinfo('__init__.py')
+        if init.is_file():
+            store['pkg_root'] = init.path
+            pkg_parts.append(node.path.name)
+            name_parts.append(node.path.name)
+            resolve_from_dir(node.get_parent())
+
+    def resolve_from_file(node: FileInfo):
+        store['pkg_root'] = node.path
+        if node.path.name != '__init__.py':
+            name_parts.append(node.path.name.pure_name)
+        resolve_from_dir(node.get_parent())
 
     if isinstance(node, str):
         node = NodeInfo.from_path(node)
         if not node:
-            raise RuntimeError(f'{node} is not exists')
+            raise FileNotFoundError(f'{node} is not exists')
 
     if node.node_type == NodeType.file:
-        return _file_find_root(node)
+        resolve_from_file(node)
     else:
-        return _find_from_dir(node)
+        resolve_from_dir(node)
+
+    store['path'] = node.path
+
+    pkg_parts.reverse()
+    name_parts.reverse()
+
+    if pkg_parts:
+        store['pkg_name'] = '.'.join(pkg_parts)
+    else:
+        store['pkg_name'] = node.path.name.pure_name
+    store['name'] = '.'.join(name_parts)
+
+    return PyInfo(**store)
+
+
+def find_pkg_root(node: Union[NodeInfo, str]):
+    '''
+    find the top level dir which has `__init__.py`, return the path of the `__init__.py`.
+    if `node` is not in a package, return the path of the `node`.
+    '''
+
+    return get_pyinfo(node).pkg_root
+
+def get_pkg_name(node: Union[NodeInfo, str]):
+    '''
+    get the package name of the `node`.
+    if `node` is not in a package, return the name of the `node`.
+    '''
+
+    return get_pyinfo(node).pkg_name
