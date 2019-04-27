b714be3202b3f97cb21347f0b156e53a4e9ce607
Cologler <skyoflw@gmail.com>
2019-04-27 23:23:48 +0800

    use runpy to run py_m

--- execode/runner.py
+++ execode/runner.py
@@ -6,6 +6,7 @@
 # ----------
 
 import sys
+import runpy
 
 from fsoopify import FileInfo, NodeInfo, NodeType
 
@@ -25,9 +26,12 @@ def run_py(path: str, globals: dict=None, locals: dict=None):
     globals['__name__'] = '__main__'
     exec(py_file.read_text(), globals, locals)
 
-def run_py_m(path: str, globals: dict=None, locals: dict=None):
+def run_py_m(path: str):
     '''
     run a dir like `python -m ?`.
+
+    `runpy.run_module()` only run the module in `sys.path`,
+    but `run_py_m` can run the module from any path.
     '''
 
     node = NodeInfo.from_path(path)
@@ -38,18 +42,13 @@ def run_py_m(path: str, globals: dict=None, locals: dict=None):
         pkg_dir = node # should be the dir which has a file __main__.py
         main_file = pkg_dir.get_fileinfo('__main__.py')
         if not main_file.is_file():
-            raise FileNotFoundError(f'{main_file.path} is not a file')
+            raise FileNotFoundError(f'{pkg_dir.path} is not a module')
     else:
         main_file = node # user may pass `pkg/__main__.py`
         pkg_dir = node.get_parent()
 
     sys.path.insert(0, pkg_dir.path.dirname)
-    if globals is None:
-        globals = {}
-    globals['__file__'] = main_file.path
-    globals['__name__'] = '__main__'
-    globals['__package__'] = pkg_dir.path.name
-    exec(main_file.read_text(), globals, locals)
+    return runpy.run_module(pkg_dir.path.name, run_name='__main__')
 
 def exec_pkg_py(path: str, globals: dict=None, locals: dict=None):
     '''
